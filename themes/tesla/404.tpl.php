46a2feabe3bfd6a1c6ad3cd8566c71e92585fe61
Stephen Lovell <hello@stephencreates.com>
2012-07-15 01:09:52 -0500

    add maintenance and 404 templates

---
+++ themes/tesla/404.tpl.php
@@ -0,0 +1,2 @@
+<h1>404 Error</h1> 
+<h2>File not found.</h2>
--- themes/tesla/maintenance.tpl.php
+++ themes/tesla/maintenance.tpl.php
@@ -1,16 +1,39 @@
-<?php
+<?php get_header(); ?>
 
-////////////////////////////////////////////////////
-
-// INDEX Template
-
-////////////////////////////////////////////////////
-
-echo 'MAINTENANCE Template (Index) <br />';
-
-get_header();
-echo '<br />The Website is currently under Maintenance <br /> We apologize for the inconvenience <br />';
-get_footer();
+<body id="page">
 
 
+<h1><a href="/" id="logo">Stephen Creates</a></h1>
 
+<?php get_block('menu'); ?>
+ 
+<section id="main-content">
+
+	<h1>Under Maintenance</h1>
+	
+	<p>The Website is currently under Maintenance <br /> 
+	We apologize for the inconvenience 
+	</p>
+	
+	
+</section>
+
+
+
+
+
+
+
+<footer role="contentinfo">
+<p>Created by Stephen Lovell</p>
+
+<!-- <p>&copy; 2012</p> -->
+
+</footer>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script> 
+<script src="<?php get_template_dir(); ?>/js/fixie.min.js"></script> 
+<script src="<?php get_template_dir(); ?>/js/bundle.js"></script> 
+</body>
+</html>
