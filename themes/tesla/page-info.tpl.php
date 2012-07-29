7c6c3c3153691ce0b313bd8e4f20f349989d3c3c
Stephen Lovell <hello@stephencreates.com>
2012-07-28 22:25:21 -0500

    fix trailing slash bug on blog page

---
+++ themes/tesla/page-info.tpl.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<body id="blog">
+
+
+<h1><a href="/" id="logo">Stephen Creates</a></h1>
+
+<?php get_partial('menu'); ?>
+ 
+<section id="main-content">
+
+	<h1>Info Template</h1>
+	
+	
+	<p>Info Content</p>
+	
+	
+</section>
+
+
+
+
+
+
+
+<footer role="contentinfo">
+<p>Created by Stephen Lovell</p>
+
+<!-- <p>&copy; 2012</p> -->
+
+</footer>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script> 
+<script src="<?php get_template_dir(); ?>/js/fixie.min.js"></script> 
+<script src="<?php get_template_dir(); ?>/js/bundle.js"></script> 
+</body>
+</html>
