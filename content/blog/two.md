9f1bf4d07e8c537480ca957fd839148188ca2a57
Stephen Lovell <hello@stephencreates.com>
2012-07-28 21:39:53 -0500

    messiest commit yet

---
+++ content/blog/two.md
@@ -0,0 +1,3 @@
+# There's more here than we need
+
+This would be another blog article. The seal, the seal. The seal is on fire.
--- themes/tesla/feed.tpl.php
+++ themes/tesla/feed.tpl.php
@@ -1,3 +1,6 @@
 <?php
 
-feed('./themes/'.$GLOBALS['theme'].'/feed.atom');
+feed_content('blog');
+
+// Add more stuff to edit here in the future.
+// For now you can just specific what content to put in the feed.
