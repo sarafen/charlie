5ec09076cb559e88fa166722d1d3b92eda1698f7
Stephen Lovell <hello@stephencreates.com>
2012-10-07 12:59:14 -0500

    directory jumbling

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if(phpversion() < 5) {
 } else {
 
 
-  require_once $_SERVER['DOCUMENT_ROOT'].'/app/charlie/bootstrap.inc'; 
+  require_once $_SERVER['DOCUMENT_ROOT'].'/app/bootstrap.inc'; 
   bootstrap_full(); 
   
 }
