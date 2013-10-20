5adc1c0676eb98ea2184da063362b5814fc9d405
Stephen Lovell <hello@stephencreates.com>
2013-10-19 21:12:55 -0500

    updating startup function call

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
 
 # let people know if they are running an unsupported version of PHP
-if(phpversion() < 5) {
+if(phpversion() < 5.3) {
   
-  die('<h1>Charlie requires PHP/5.0 or higher.<br>You are currently running PHP/'.phpversion().'.</h1><p>You should contact your host to see if they can upgrade your version of PHP.</p>');
+  die('<h1>Charlie requires PHP/5.3 or higher.<br>You are currently running PHP/'.phpversion().'.</h1><p>You should contact your host to see if they can upgrade your version of PHP.</p>');
 
 } else {
 
-
-  require_once $_SERVER['DOCUMENT_ROOT'].'/app/bootstrap.inc'; 
-  bootstrap_full(); 
+  require_once $_SERVER['DOCUMENT_ROOT'].'/app/startup.php'; 
+  startup_full(); 
   
 }
